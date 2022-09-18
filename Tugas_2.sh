54050f0f4511f3a3f9a7744af691dbee72749ed4
ZulfazRefie <113273803+ZulfazRefie@users.noreply.github.com>
2022-09-18 20:01:10 +0700

    Add files via upload

---
+++ Tugas_2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+
+email="me@gmail.com"
+pass="secret"
+
+read -p "Email : " logmail
+read -sp "Password : " logpass
+echo
+
+if [ $email == $logmail ] && [ $pass == $logpass ]
+then
+  echo "Login sukses"
+else 
+  echo "Email atau password salah"
+fi
